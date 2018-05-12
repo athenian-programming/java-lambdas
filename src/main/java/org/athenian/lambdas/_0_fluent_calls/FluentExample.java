e43b96630aadd07fbc2b1c5fc383a98a2ee045ae
Paul Ambrose <pambrose@mac.com>
2018-05-12 11:03:06 -0700

    Update comments

--- src/main/java/org/athenian/lambdas/_0_fluent_calls/FluentExample.java
+++ src/main/java/org/athenian/lambdas/_0_fluent_calls/FluentExample.java
@@ -5,6 +5,22 @@ public class FluentExample {
   public static void main(String[] args) {
 
     // Non-fluent calls
+    String str1 = "This is a test";
+    str1 = str1.toUpperCase();
+    str1 = str1.replaceAll("IS", "99");
+    str1 = str1.substring(0, 8);
+    System.out.println(str1);
+
+    // Fluent calls
+    String str2 =
+        "This is a test"
+            .toUpperCase()
+            .replaceAll("IS", "99")
+            .substring(0, 8);
+    System.out.println(str2);
+
+
+    // Non-fluent calls
     Student bill = new Student();
     bill.setFirstName("Bill");
     bill.setLastName("Jones");
